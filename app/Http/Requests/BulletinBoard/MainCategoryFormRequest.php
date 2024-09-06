60228c051e510262fab75e4bfce724ae4f267e84
伏屋優菜 <>
2024-09-06 19:35:09 +0900

    main_cateroryのバリデーションエラー修正

--- app/Http/Requests/BulletinBoard/MainCategoryFormRequest.php
+++ app/Http/Requests/BulletinBoard/MainCategoryFormRequest.php
@@ -24,7 +24,7 @@ class MainCategoryFormRequest extends FormRequest
     public function rules()
     {
         return [
-            'main_category_name' => 'required|string|max:100|unique:main_categories,main_category_name',
+            'main_category_name' => 'required|string|max:100|unique:main_categories,main_category',
         ];
     }
 
