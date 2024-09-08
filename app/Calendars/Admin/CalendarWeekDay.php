b8a7d4eea961903d9cfd398096e8fe6684571015
伏屋優菜 <>
2024-09-08 19:59:27 +0900

    人数表示

--- app/Calendars/Admin/CalendarWeekDay.php
+++ app/Calendars/Admin/CalendarWeekDay.php
@@ -1,86 +1,103 @@
 <?php
+
 namespace App\Calendars\Admin;
 
 use Carbon\Carbon;
 use App\Models\Calendars\ReserveSettings;
 
-class CalendarWeekDay{
-  protected $carbon;
+class CalendarWeekDay
+{
+    protected $carbon;
 
-  function __construct($date){
-    $this->carbon = new Carbon($date);
-  }
-
-  function getClassName(){
-    return "day-" . strtolower($this->carbon->format("D"));
-  }
-
-  function render(){
-    return '<p class="day">' . $this->carbon->format("j") . '日</p>';
-  }
-
-  function everyDay(){
-    return $this->carbon->format("Y-m-d");
-  }
-
-  function dayPartCounts($ymd){
-    $html = [];
-    $one_part = ReserveSettings::with('users')->where('setting_reserve', $ymd)->where('setting_part', '1')->first();
-    $two_part = ReserveSettings::with('users')->where('setting_reserve', $ymd)->where('setting_part', '2')->first();
-    $three_part = ReserveSettings::with('users')->where('setting_reserve', $ymd)->where('setting_part', '3')->first();
-
-    $html[] = '<div class="text-left">';
-    if($one_part){
-      $html[] = '<p class="day_part m-0 pt-1">1部</p>';
+    function __construct($date)
+    {
+        $this->carbon = new Carbon($date);
     }
-    if($two_part){
-      $html[] = '<p class="day_part m-0 pt-1">2部</p>';
-    }
-    if($three_part){
-      $html[] = '<p class="day_part m-0 pt-1">3部</p>';
-    }
-    $html[] = '</div>';
 
-    return implode("", $html);
-  }
+    function getClassName()
+    {
+        return "day-" . strtolower($this->carbon->format("D"));
+    }
+
+    function render()
+    {
+        return '<p class="day">' . $this->carbon->format("j") . '日</p>';
+    }
+
+    function everyDay()
+    {
+        return $this->carbon->format("Y-m-d");
+    }
+
+    function dayPartCounts($ymd)
+    {
+        $html = [];
+        $one_part = ReserveSettings::with('users')->where('setting_reserve', $ymd)->where('setting_part', '1')->first();
+        $two_part = ReserveSettings::with('users')->where('setting_reserve', $ymd)->where('setting_part', '2')->first();
+        $three_part = ReserveSettings::with('users')->where('setting_reserve', $ymd)->where('setting_part', '3')->first();
+
+        $html[] = '<div class="text-left">';
+        if ($one_part) {
+            // $html[] = '<p class="day_part m-0 pt-1">1部:</p>';
+            $one_part_count = $one_part->users->count();
+            $html[] = '<p class="day_part m-0 pt-1">1部: ' . $one_part_count . ' </p>';
+        }
+        if ($two_part) {
+            // $html[] = '<p class="day_part m-0 pt-1">2部</p>';
+            $two_part_count = $two_part->users->count();
+            $html[] = '<p class="day_part m-0 pt-1">2部: ' . $two_part_count . ' </p>';
+        }
+        if ($three_part) {
+            // $html[] = '<p class="day_part m-0 pt-1">3部</p>';
+            $three_part_count = $three_part->users->count();
+            $html[] = '<p class="day_part m-0 pt-1">3部: ' . $three_part_count . ' </p>';
+        }
+        $html[] = '</div>';
+
+        return implode("", $html);
+    }
 
 
-  function onePartFrame($day){
-    $one_part_frame = ReserveSettings::where('setting_reserve', $day)->where('setting_part', '1')->first();
-    if($one_part_frame){
-      $one_part_frame = ReserveSettings::where('setting_reserve', $day)->where('setting_part', '1')->first()->limit_users;
-    }else{
-      $one_part_frame = "20";
+    function onePartFrame($day)
+    {
+        $one_part_frame = ReserveSettings::where('setting_reserve', $day)->where('setting_part', '1')->first();
+        if ($one_part_frame) {
+            $one_part_frame = ReserveSettings::where('setting_reserve', $day)->where('setting_part', '1')->first()->limit_users;
+        } else {
+            $one_part_frame = "20";
+        }
+        return $one_part_frame;
     }
-    return $one_part_frame;
-  }
-  function twoPartFrame($day){
-    $two_part_frame = ReserveSettings::where('setting_reserve', $day)->where('setting_part', '2')->first();
-    if($two_part_frame){
-      $two_part_frame = ReserveSettings::where('setting_reserve', $day)->where('setting_part', '2')->first()->limit_users;
-    }else{
-      $two_part_frame = "20";
+    function twoPartFrame($day)
+    {
+        $two_part_frame = ReserveSettings::where('setting_reserve', $day)->where('setting_part', '2')->first();
+        if ($two_part_frame) {
+            $two_part_frame = ReserveSettings::where('setting_reserve', $day)->where('setting_part', '2')->first()->limit_users;
+        } else {
+            $two_part_frame = "20";
+        }
+        return $two_part_frame;
     }
-    return $two_part_frame;
-  }
-  function threePartFrame($day){
-    $three_part_frame = ReserveSettings::where('setting_reserve', $day)->where('setting_part', '3')->first();
-    if($three_part_frame){
-      $three_part_frame = ReserveSettings::where('setting_reserve', $day)->where('setting_part', '3')->first()->limit_users;
-    }else{
-      $three_part_frame = "20";
+    function threePartFrame($day)
+    {
+        $three_part_frame = ReserveSettings::where('setting_reserve', $day)->where('setting_part', '3')->first();
+        if ($three_part_frame) {
+            $three_part_frame = ReserveSettings::where('setting_reserve', $day)->where('setting_part', '3')->first()->limit_users;
+        } else {
+            $three_part_frame = "20";
+        }
+        return $three_part_frame;
     }
-    return $three_part_frame;
-  }
 
-  //
-  function dayNumberAdjustment(){
-    $html = [];
-    $html[] = '<div class="adjust-area">';
-    $html[] = '<p class="d-flex m-0 p-0">1部<input class="w-25" style="height:20px;" name="1" type="text" form="reserveSetting"></p>';
-    $html[] = '<p class="d-flex m-0 p-0">2部<input class="w-25" style="height:20px;" name="2" type="text" form="reserveSetting"></p>';
-    $html[] = '<p class="d-flex m-0 p-0">3部<input class="w-25" style="height:20px;" name="3" type="text" form="reserveSetting"></p>';
-    $html[] = '</div>';
-    return implode('', $html);
-  }
+    //
+    function dayNumberAdjustment()
+    {
+        $html = [];
+        $html[] = '<div class="adjust-area">';
+        $html[] = '<p class="d-flex m-0 p-0">1部<input class="w-25" style="height:20px;" name="1" type="text" form="reserveSetting"></p>';
+        $html[] = '<p class="d-flex m-0 p-0">2部<input class="w-25" style="height:20px;" name="2" type="text" form="reserveSetting"></p>';
+        $html[] = '<p class="d-flex m-0 p-0">3部<input class="w-25" style="height:20px;" name="3" type="text" form="reserveSetting"></p>';
+        $html[] = '</div>';
+        return implode('', $html);
+    }
 }
--- app/Http/Controllers/Authenticated/Calendar/Admin/CalendarsController.php
+++ app/Http/Controllers/Authenticated/Calendar/Admin/CalendarsController.php
@@ -14,29 +14,34 @@ use DB;
 
 class CalendarsController extends Controller
 {
-    public function show(){
+    public function show()
+    {
         $calendar = new CalendarView(time());
         return view('authenticated.calendar.admin.calendar', compact('calendar'));
     }
 
-    public function reserveDetail($date, $part){
+    public function reserveDetail($date, $part)
+    {
         $reservePersons = ReserveSettings::with('users')->where('setting_reserve', $date)->where('setting_part', $part)->get();
+
         return view('authenticated.calendar.admin.reserve_detail', compact('reservePersons', 'date', 'part'));
     }
 
-    public function reserveSettings(){
+    public function reserveSettings()
+    {
         $calendar = new CalendarSettingView(time());
         return view('authenticated.calendar.admin.reserve_setting', compact('calendar'));
     }
 
-    public function updateSettings(Request $request){
+    public function updateSettings(Request $request)
+    {
         $reserveDays = $request->input('reserve_day');
-        foreach($reserveDays as $day => $parts){
-            foreach($parts as $part => $frame){
+        foreach ($reserveDays as $day => $parts) {
+            foreach ($parts as $part => $frame) {
                 ReserveSettings::updateOrCreate([
                     'setting_reserve' => $day,
                     'setting_part' => $part,
-                ],[
+                ], [
                     'setting_reserve' => $day,
                     'setting_part' => $part,
                     'limit_users' => $frame,
@@ -45,4 +50,30 @@ class CalendarsController extends Controller
         }
         return redirect()->route('calendar.admin.setting', ['user_id' => Auth::id()]);
     }
+
+    // public function reserveCounts($date)
+    // {
+    //     // 1部、2部、3部それぞれの予約人数をカウント
+    //     $onePartCount = ReserveSettings::where('setting_reserve', $date)
+    //         ->where('setting_part', 1)
+    //         ->withCount('users')
+    //         ->first()?->users_count ?? 0;
+
+    //     $twoPartCount = ReserveSettings::where('setting_reserve', $date)
+    //         ->where('setting_part', 2)
+    //         ->withCount('users')
+    //         ->first()?->users_count ?? 0;
+
+    //     $threePartCount = ReserveSettings::where('setting_reserve', $date)
+    //         ->where('setting_part', 3)
+    //         ->withCount('users')
+    //         ->first()?->users_count ?? 0;
+
+    //     // 結果を配列として返す
+    //     return [
+    //         'onePartCount' => $onePartCount,
+    //         'twoPartCount' => $twoPartCount,
+    //         'threePartCount' => $threePartCount,
+    //     ];
+    // }
 }
